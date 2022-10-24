659865e90d5b62a988423aef839f9a140eb75e98
KorkinIgor <igorkorkin55@gmail.com>
2022-10-24 22:04:55 +0600

    пкуоф

---
+++ Assets/newajerg.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newajerg : MonoBehaviour
+{
+    
+    void Start()
+    {
+      print("привет");  
+    }
+
+}
